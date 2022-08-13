c1ba6af46f9b072f32ba6a8680410a5a80e61f71
waleedfarajallah <97733463+waleedfarajallah@users.noreply.github.com>
2022-08-13 19:59:29 +0300

    Add files via upload
    Enigma (Ciphering/Dciphering) app

---
+++ enigma/index.js
@@ -0,0 +1,32 @@
+'use strict';
+const crypto = require('crypto');
+const qr = require('qr-image');
+const fs = require('fs');
+
+module.exports = function(key) {
+    this.key = key;
+    return {
+        encode: (str) => {
+            let encoder = crypto.createCipher('aes-256-ctr', this.key);
+            return encoder.update(str, 'utf8', 'hex');
+        },
+        decode: (str) => {
+            let decoder = crypto.createDecipher('aes-256-ctr', this.key);
+            return decoder.update(str, 'hex', 'utf8');
+        },
+        qrgen: (data, file) => {
+            let dataToEncode = data || null;
+            let outImage = file || null;
+
+            if (dataToEncode !== null && outImage !== null) {
+                qr.image(dataToEncode, {
+                    'type': 'png',
+                    size: 20
+                }).pipe(fs.createWriteStream(outImage));
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
